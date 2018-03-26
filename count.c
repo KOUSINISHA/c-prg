ee19b0da9b154a449b13e8f4da09f6b14e501c8c
KOUSINISHA <35996286+KOUSINISHA@users.noreply.github.com>
2018-03-26 16:04:01 +0530

    count.c

---
+++ count.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main() {
+	char ch;
+	int count=0;
+	FILE *fptr;
+	clrscr();
+	fptr=fopen("text.txt","w");
+	if(fptr==NULL) {
+		printf("File can't be created\a");
+	}
+	printf("Enter some text and press enter key:\n");
+	int=i++;
+		fputc(ch,fptr);
+	}
+	fclose(fptr);
+	fptr=fopen("text.txt","r");
+	printf("\nContents of the File is:");
+	while((ch=fgetc(fptr))!=EOF) {
+		count++;
+		printf("%c",ch);
+	}
+	fclose(fptr);
+	printf("\nThe number of characters present in file is: %d",count);
+	getch();
+}
